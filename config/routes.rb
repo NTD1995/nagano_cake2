6e9390ceedde6be5854a375478e8775b5763df18
abedaiki <abe12261994@gmail.com>
2025-06-17 11:00:53 +0000

    Add 会員側のログイン、新規登録機能、管理者側のログイン機能のルーティング、ビュー、コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # トップページ
   root to: "public/homes#top"
+  # アバウトページ
   get "/about", to: "public/homes#about", as: "about"
+  # 管理者のログイン
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 管理者トップページ 
+  get "/admin", to: "admin/homes#top", as: "admin/top"
+
+  # 会員のログイン、新規登録
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }  
 end
