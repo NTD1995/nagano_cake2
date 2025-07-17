cc10502881b101381eae98aa1280efd95a057be6
abedaiki <abe12261994@gmail.com>
2025-07-17 12:23:56 +0000

    Fix 新規登録の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_customer
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,21 @@ class ApplicationController < ActionController::Base
     if customer_signed_in?
       @customer = current_customer
     end
-  end  
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :last_name, :first_name,
+      :last_name_kana, :first_name_kana,
+      :post_code, :address, :phone_number
+    ])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :last_name, :first_name,
+      :last_name_kana, :first_name_kana,
+      :post_code, :address, :phone_number
+    ])
+  end
 end
