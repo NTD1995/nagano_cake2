148ea3031f0000cf529e2f73686fa6c07d4f1d45
abedaiki <abe12261994@gmail.com>
2025-08-24 10:04:25 +0000

    Add よくある質問のモデル、テーブルの作成

---
+++ db/migrate/20250824100113_create_faqs.rb
@@ -0,0 +1,10 @@
+class CreateFaqs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :faqs do |t|
+      t.string :question, null: false
+      t.text :answer, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_08_11_132837) do
+ActiveRecord::Schema.define(version: 2025_08_24_100113) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -122,6 +122,13 @@ ActiveRecord::Schema.define(version: 2025_08_11_132837) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "faqs", force: :cascade do |t|
+    t.string "question", null: false
+    t.text "answer", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "favorites", force: :cascade do |t|
